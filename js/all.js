dc3b532f128887a3013b484b989451371e5d6b67
KalpasWang <48082968+KalpasWang@users.noreply.github.com>
2020-11-12 16:34:09 +0800

    重構 CSS 改為 Sass 與使用 BEM，header 區塊完成

--- js/all.js
+++ js/all.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-  $('.menu-bars').click(function(){
-    $('nav').toggleClass('menu-show');
-    $('.fa-times').toggleClass('d-none');
-    $('.fa-bars').toggleClass('d-none');
+  $('.nav__mobile-menu-bars').click(function(){
+    $('.nav__menu').toggleClass('menu-show');
+    $('.fa-times').toggleClass('h-hide');
+    $('.fa-bars').toggleClass('h-hide');
   })
 });
