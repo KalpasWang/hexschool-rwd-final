27b17c0e36cf9c32e267a654472893f62f9ffe39
KalpasWang <48082968+KalpasWang@users.noreply.github.com>
2020-11-15 17:01:30 +0800

    cart 頁面重構完成

--- js/all.js
+++ js/all.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
+
+  // 選單收合
   $('.nav__mobile-menu-bars').click(function(){
     $('.nav__menu').toggleClass('menu-show');
     $('.fa-times').toggleClass('h-hide');
     $('.fa-bars').toggleClass('h-hide');
+  })
+
+  // 產品加入我的最愛
+  $('.fa-heart').click(function(){
+    $(this).toggleClass('h-icon-fill');
   })
 });
