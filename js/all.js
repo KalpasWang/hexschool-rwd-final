df8df5498cb075101ace688f8619e0e61902c225
KalpasWang <48082968+KalpasWang@users.noreply.github.com>
2020-11-17 16:38:50 +0800

    重構完成，小細節調整

--- js/all.js
+++ js/all.js
@@ -11,4 +11,34 @@ $(document).ready(function() {
   $('.fa-heart').click(function(){
     $(this).toggleClass('h-icon-fill');
   })
+
+  // 滑動到餐廳特色區域
+  $('.nav__menu-link[href="index.html#feature"').click(function(event) {
+    if($(document).attr('title').includes('首頁')) {
+      event.preventDefault();      
+      $('html,body').animate({
+        scrollTop: $('#feature').offset().top
+      }, 500);
+    }
+  });
+
+  // 滑動到餐廳主廚區域
+  $('.nav__menu-link[href="index.html#main-chief"').click(function(event) {
+    if($(document).attr('title').includes('首頁')) {
+      event.preventDefault();      
+      $('html,body').animate({
+        scrollTop: $('#main-chief').offset().top
+      }, 500);
+    }
+  });
+
+  // 滑動到餐廳訂位區域
+  $('.nav__menu-link[href="index.html#ordering"').click(function(event) {
+    if($(document).attr('title').includes('首頁')) {
+      event.preventDefault();      
+      $('html,body').animate({
+        scrollTop: $('#ordering').offset().top
+      }, 500);
+    }
+  });
 });
